88f174ac4123ee533ab21d461f99296d25928938
Andrew Nesbitt <andrewnez@gmail.com>
2024-01-04 18:18:56 +0000

    Refactor list sorting and add project count to list view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,18 @@
 class ListsController < ApplicationController
   def index
-    scope = List.with_repository.with_readme.where.not(projects_count: nil).where('projects_count > 0').order(projects_count: :desc)
+    scope = List.with_repository.with_readme.where.not(projects_count: nil).where('projects_count > 50')
+
+    if params[:sort].present? || params[:order].present?
+      sort = params[:sort].presence || 'updated_at'
+      if params[:order] == 'asc'
+        scope = scope.order(Arel.sql(sort).asc.nulls_last)
+      else
+        scope = scope.order(Arel.sql(sort).desc.nulls_last)
+      end
+    else
+      scope = scope.order(@sort = Arel.sql("(repository ->> 'stargazers_count')::text::integer").desc.nulls_last)
+    end
+
     @pagy, @lists = pagy(scope)
   end
 
