99858b0da05b8ab1b1c9c273bbb3becf2919de8f
Andrew Nesbitt <andrewnez@gmail.com>
2024-01-06 21:12:32 +0000

    Update Faraday configuration to include timeouts and request encoding

--- config/initializers/faraday.rb
+++ config/initializers/faraday.rb
@@ -1,2 +1,12 @@
 require 'faraday/typhoeus'
 Faraday.default_adapter = :typhoeus
+
+Faraday.default_connection = Faraday::Connection.new do |builder|
+  builder.response :follow_redirects
+  builder.request :url_encoded
+  builder.adapter Faraday.default_adapter
+
+  # Set timeouts
+  builder.options.timeout = 5           # open/read timeout in seconds
+  builder.options.open_timeout = 2      # connection open timeout in seconds
+end
