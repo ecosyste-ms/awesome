53c208141db0b5024b80ae06df77a3176ab09b56
Andrew Nesbitt <andrewnez@gmail.com>
2024-07-30 13:51:07 +0100

    Fix test

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -2,12 +2,7 @@ require "test_helper"
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get topics_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get topics_show_url
+    get topics_url
     assert_response :success
   end
 end
