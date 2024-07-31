2f946abd8d1fce1efa93a4ad747ec393fa79e7ad
Andrew Nesbitt <andrewnez@gmail.com>
2024-07-31 13:04:00 +0100

    Display categories and subcategories in Lists#show

--- app/models/list_project.rb
+++ app/models/list_project.rb
@@ -1,4 +1,16 @@
 class ListProject < ApplicationRecord
   belongs_to :list
   belongs_to :project
+
+  def to_s
+    project.try(:name) || name
+  end
+
+  def url
+    project.try(:url) || find_url
+  end
+
+  def find_url
+    list.readme_links.first{|link| link[:name] == name}[:url]
+  end
 end
