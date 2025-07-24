43c7a5367b7d9332f3cac36c4a61a1231e908306
Andrew Nesbitt <andrewnez@gmail.com>
2025-07-24 09:40:06 +0100

    Set statement timeout before and after changing stars column type to bigint

--- db/migrate/20250724082324_change_stars_to_bigint_in_projects.rb
+++ db/migrate/20250724082324_change_stars_to_bigint_in_projects.rb
@@ -1,6 +1,8 @@
 class ChangeStarsToBigintInProjects < ActiveRecord::Migration[8.0]
   def change
+    execute "SET statement_timeout = 0"
     change_column :projects, :stars, :bigint, default: 0
     change_column :lists, :stars, :bigint, default: 0
+    execute "SET statement_timeout = '30s'"
   end
 end
