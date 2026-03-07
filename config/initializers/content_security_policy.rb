f33bce3ae223fc111df49d4f2d6c9c89df787cb2
Andrew Nesbitt <andrewnez@gmail.com>
2026-03-07 08:57:32 +0000

    Add ethicalads support
    Add ethicalads client script, CSP entries for media.ethicalads.io and
    server.ethicalads.io, and fixed footer ad placement.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,10 +1,10 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('self'),
-    script_src: %w('self' 'unsafe-inline' https://cdnjs.cloudflare.com),
+    script_src: %w('self' 'unsafe-inline' https://cdnjs.cloudflare.com https://media.ethicalads.io https://server.ethicalads.io),
     style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com),
     font_src: %w('self' https://fonts.gstatic.com),
     img_src: %w('self' data: https:),
-    connect_src: %w('self' *.ecosyste.ms)
+    connect_src: %w('self' *.ecosyste.ms https://server.ethicalads.io)
   }
 end
