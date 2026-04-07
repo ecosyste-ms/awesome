7e3c693aa75b5f942f853d9697160a6f63905079
Andrew Nesbitt <andrewnez@gmail.com>
2026-04-07 12:00:26 +0100

    Remove EthicalAds integration

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,10 +1,10 @@
 SecureHeaders::Configuration.default do |config|
   config.csp = {
     default_src: %w('self'),
-    script_src: %w('self' 'unsafe-inline' https://cdnjs.cloudflare.com https://media.ethicalads.io https://server.ethicalads.io),
+    script_src: %w('self' 'unsafe-inline' https://cdnjs.cloudflare.com),
     style_src: %w('self' 'unsafe-inline' https://fonts.googleapis.com),
     font_src: %w('self' https://fonts.gstatic.com),
     img_src: %w('self' data: https:),
-    connect_src: %w('self' *.ecosyste.ms https://server.ethicalads.io)
+    connect_src: %w('self' *.ecosyste.ms)
   }
 end
